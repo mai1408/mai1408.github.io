55dca557c43695f5293199e63c779a751d9f0bdf
Antonios Dervisis <mai1408@uom.edu.gr>
2015-07-03 19:01:49 +0300

    PHP fix

--- html_form_send.php
+++ html_form_send.php
@@ -67,5 +67,5 @@ if(isset($_POST['email'])) {
 $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
+@mail($email_to, $email_subject, $email_message, $headers);  }
 ?>
